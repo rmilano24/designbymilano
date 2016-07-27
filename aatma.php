b83bee09d7b8910dd7e5a42496be76cc2698f6b0
Ryan Milano <ryan@conveyedconcepts.com>
2016-07-26 17:59:03 -0700

    update single portfolio layout

--- aatma.php
+++ aatma.php
@@ -21,39 +21,170 @@
 
 
 
-            <!-- <div class="container">
+            <section class="full-section" style="padding-bottom:0;">
+      				<div class="full-section-container">
+
+      					<div class="rotate-links">
+
+      						<a href="#">Facebook</a>
+      						<a href="#">Twitter</a>
+      						<a href="#">LinkedIn</a>
+
+      					</div><!-- rotate-links -->
+
+      					<div class="container-fluid">
+      						<div class="row">
+      							<div class="col-sm-offset-1 col-sm-11">
+
+      								<div class="owl-carousel images-slider aatma">
+      									<div class="item">
+      										<!-- <img src="images/portfolio/slider/image-1.jpg" alt=""> -->
+      									</div><!-- item -->
+      								</div><!-- images-slider -->
+
+
+      							</div><!-- col -->
+      						</div><!-- row -->
+      					</div><!-- container-fluid -->
+
+      				</div><!-- full-section-container -->
+      			</section><!-- section-slider -->
+
+            <div class="container">
       				<div class="row">
-      					<div class="col-sm-3">
+      					<div class="col-md-4 col-sm-6">
+
+      						<h4 class="text-default-color">Services</h4>
+
+      						<br>
+
+      						<p><span class="text-uppercase"><strong>Name: </strong></span> 3d Project of an object<p>
+      						<p><span class="text-uppercase"><strong>Start Date: </strong></span> Jan. 14, 2016<p>
+      						<p><span class="text-uppercase"><strong>End Date: </strong></span> Jan. 30, 2016<p>
+
+      					</div><!-- col -->
+      					<div class="col-md-8 col-sm-6">
+
+      						<h4 class="text-default-color">A fantastic road</h4>
+
+      						<br>
+
+      						<p>Suspendisse metus turpis, blandit sed dolor quis, commodo commodo elit. Sed sagittis mollis ligula eget rhoncus. Nam blandit
+      						pellentesque odio.Sus pendisse metus turpis, blandit sed dolor quis, com modo commodo elit. Sed sagittis mollis.</p>
+
+
+
+      					</div><!-- col -->
+
+      				</div><!-- row -->
+      			</div><!-- container -->
+
+            <div class="container">
+      				<div class="row">
+      					<div class="col-sm-12">
+
+      						<div class="portfolio-item portfolio-creative thumbnail-left">
+
+      							<div class="row">
+      								<div class="col-sm-4">
+
+      									<div class="portfolio-item-details">
+
+      										<h4><a href="portfolio-single.html">Lamp design</a></h4>
+      										<h6>Campaigns</h6>
+
+      										<p>Phasellus in ante ac tortor ali quam mollis in sit amet libero. Vesti bulum a accums.</p>
+
+      									</div><!-- portfolio-item-details -->
+
+      								</div><!-- col -->
+      								<div class="col-sm-8">
+
+      									<div class="portfolio-item-thumbnail">
+
+      										<img src="images/portfolio/image-49.jpg" alt="">
+
+      										<div class="portfolio-item-hover">
+
+      											<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-49.jpg">+</a>
+
+      										</div><!-- portfolio-item-hover -->
+
+      									</div><!-- portfolio-item-thumbnail -->
+
+      								</div><!-- col -->
+      							</div><!-- row -->
+
+      						</div><!-- portfolio-item -->
+
+      					</div><!-- col -->
+      				</div><!-- row -->
+      			</div><!-- container -->
+
+      			<br><br><br>
+
+      			<div class="container">
+      				<div class="row">
+      					<div class="col-md-4 col-sm-6">
+
+      						<div class="portfolio-item">
+
+      							<div class="portfolio-item-thumbnail">
+
+      								<img src="images/portfolio/image-27.jpg" alt="">
+
+      								<div class="portfolio-item-hover">
+
+      									<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-27.jpg">+</a>
+
+      								</div><!-- portfolio-item-hover -->
+
+      							</div><!-- portfolio-item-thumbnail -->
+
+      						</div><!-- portfolio-item -->
+
+      					</div><!-- col -->
+      					<div class="col-md-4 col-sm-6">
+
+      						<div class="portfolio-item">
+
+      							<div class="portfolio-item-thumbnail">
+
+      								<img src="images/portfolio/image-20.jpg" alt="">
+
+      								<div class="portfolio-item-hover">
+
+      									<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-20.jpg">+</a>
+
+      								</div><!-- portfolio-item-hover -->
+
+      							</div><!-- portfolio-item-thumbnail -->
+
+      						</div><!-- portfolio-item -->
+
+      					</div><!-- col -->
+      					<div class="col-md-4 col-sm-12">
 
       						<div class="headline">
 
-      							<h1>The Challenge</h1>
+      							<h3>We have more <br> projects</h3>
+      							<h6>Industrial Design</h6>
 
-      						</div>
+      						</div><!-- headline -->
 
-      					</div>
-      					<div class="col-sm-6">
+      						<br>
 
-      						<p>Wherever you are in the world, you understand that creative design is one of the most important factors in helping your business reach it’s true potential. We work as an extension of your team to create brands, illustration and delightfully intuitive, user-focused design with proven results.</p>
+      						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultrices a magna nec posuere. Duis porttitor lacus nulla, at
+      						tincidunt sapien dapibus in phasellus vehicula convallis est.</p>
 
-      					</div>
-                <div class="col-sm-3">
+      						<br>
 
-      						<div class="headline">
+      						<a class="btn btn-black" href="#">Discover more</a>
 
-      							<h4>Capabilites Flexed</h4>
-                    <ul>
-                      <li><a href="#">UI/UX Design</a></li>
-                      <li><a href="#">Development</a></li>
-                    </ul>
+      					</div><!-- col -->
+      				</div><!-- row -->
+      			</div><!-- container -->
 
-      						</div>
-
-      					</div>
-      				</div>
-      			</div>
-
-      			<br><br> -->
 
 			<div class="container hero-work">
 				<div class="row">
@@ -64,6 +195,8 @@
 					</div><!-- col -->
 				</div><!-- row -->
 			</div><!-- container -->
+
+
       </div><!-- PAGE CONTENT -->
 
       <a href="the-barre-studio.php">
---
+++ portfolio-single.php
@@ -0,0 +1,212 @@
+<?php include 'inc/header.php'; ?>
+
+
+        <!-- CONTENT -->
+        <div id="page-content">
+
+			<div id="page-header">
+                <div class="container">
+                    <div class="row">
+						<div class="col-sm-3">
+
+							<div class="page-header-simbol">P</div>
+
+						</div><!-- col -->
+                        <div class="col-sm-9">
+
+                            <h2>Portfolio</h2>
+
+                        </div><!-- col -->
+                    </div><!-- row -->
+                </div><!-- container -->
+            </div><!-- page-header -->
+
+			<section class="full-section" style="padding-bottom:0;">
+				<div class="full-section-container">
+
+					<div class="rotate-links">
+
+						<a href="#">Facebook</a>
+						<a href="#">Twitter</a>
+						<a href="#">LinkedIn</a>
+
+					</div><!-- rotate-links -->
+
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-offset-1 col-sm-11">
+
+								<div class="owl-carousel images-slider">
+									<div class="item">
+										<!-- <img src="images/portfolio/slider/image-1.jpg" alt=""> -->
+									</div><!-- item -->
+								</div><!-- images-slider -->
+
+
+							</div><!-- col -->
+						</div><!-- row -->
+					</div><!-- container-fluid -->
+
+				</div><!-- full-section-container -->
+			</section><!-- section-slider -->
+
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+
+						<div class="headline">
+
+							<h3>Project of a minimalistic lamp</h3>
+							<h6>Industrial Design</h6>
+
+						</div><!-- headline -->
+
+					</div><!-- col -->
+				</div><!-- row -->
+			</div><!-- container -->
+
+			<br>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-6">
+
+						<h4 class="text-default-color">Details</h4>
+
+						<br>
+
+						<p><span class="text-uppercase"><strong>Name: </strong></span> 3d Project of an object<p>
+						<p><span class="text-uppercase"><strong>Start Date: </strong></span> Jan. 14, 2016<p>
+						<p><span class="text-uppercase"><strong>End Date: </strong></span> Jan. 30, 2016<p>
+						<p><span class="text-uppercase"><strong>Company: </strong></span> MiloThemes<p>
+						<p><span class="text-uppercase"><strong>Category: </strong></span> 3d, design, web design<p>
+						<p><span class="text-uppercase"><strong>Web: </strong></span> <a href="#">www.milothemes.com</a><p>
+						<p><span class="text-uppercase"><strong>Tags: </strong></span> <a href="#">web</a>, <a href="#">design</a>, <a href="#">object</a>, <a href="#">3d</a><p>
+
+					</div><!-- col -->
+					<div class="col-md-8 col-sm-6">
+
+						<h4 class="text-default-color">A fantastic road</h4>
+
+						<br>
+
+						<p>Suspendisse metus turpis, blandit sed dolor quis, commodo commodo elit. Sed sagittis mollis ligula eget rhoncus. Nam blandit
+						pellentesque odio.Sus pendisse metus turpis, blandit sed dolor quis, com modo commodo elit. Sed sagittis mollis.</p>
+
+
+
+					</div><!-- col -->
+					
+				</div><!-- row -->
+			</div><!-- container -->
+
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+
+						<div class="portfolio-item portfolio-creative thumbnail-left">
+
+							<div class="row">
+								<div class="col-sm-4">
+
+									<div class="portfolio-item-details">
+
+										<h4><a href="portfolio-single.html">Lamp design</a></h4>
+										<h6>Campaigns</h6>
+
+										<p>Phasellus in ante ac tortor ali quam mollis in sit amet libero. Vesti bulum a accums.</p>
+
+									</div><!-- portfolio-item-details -->
+
+								</div><!-- col -->
+								<div class="col-sm-8">
+
+									<div class="portfolio-item-thumbnail">
+
+										<img src="images/portfolio/image-49.jpg" alt="">
+
+										<div class="portfolio-item-hover">
+
+											<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-49.jpg">+</a>
+
+										</div><!-- portfolio-item-hover -->
+
+									</div><!-- portfolio-item-thumbnail -->
+
+								</div><!-- col -->
+							</div><!-- row -->
+
+						</div><!-- portfolio-item -->
+
+					</div><!-- col -->
+				</div><!-- row -->
+			</div><!-- container -->
+
+			<br><br><br>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-6">
+
+						<div class="portfolio-item">
+
+							<div class="portfolio-item-thumbnail">
+
+								<img src="images/portfolio/image-27.jpg" alt="">
+
+								<div class="portfolio-item-hover">
+
+									<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-27.jpg">+</a>
+
+								</div><!-- portfolio-item-hover -->
+
+							</div><!-- portfolio-item-thumbnail -->
+
+						</div><!-- portfolio-item -->
+
+					</div><!-- col -->
+					<div class="col-md-4 col-sm-6">
+
+						<div class="portfolio-item">
+
+							<div class="portfolio-item-thumbnail">
+
+								<img src="images/portfolio/image-20.jpg" alt="">
+
+								<div class="portfolio-item-hover">
+
+									<a class="fancybox zoom-action" data-fancybox-group="portfolio" href="images/portfolio/image-20.jpg">+</a>
+
+								</div><!-- portfolio-item-hover -->
+
+							</div><!-- portfolio-item-thumbnail -->
+
+						</div><!-- portfolio-item -->
+
+					</div><!-- col -->
+					<div class="col-md-4 col-sm-12">
+
+						<div class="headline">
+
+							<h3>We have more <br> projects</h3>
+							<h6>Industrial Design</h6>
+
+						</div><!-- headline -->
+
+						<br>
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultrices a magna nec posuere. Duis porttitor lacus nulla, at
+						tincidunt sapien dapibus in phasellus vehicula convallis est.</p>
+
+						<br>
+
+						<a class="btn btn-black" href="#">Discover more</a>
+
+					</div><!-- col -->
+				</div><!-- row -->
+			</div><!-- container -->
+
+        </div><!-- PAGE CONTENT -->
+
+
+      <?php include 'inc/footer.php'; ?>
