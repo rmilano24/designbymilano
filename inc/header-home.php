f0fdf2ac265ef6728f7d175f6e97a85de511f999
Ryan Milano <ryan@conveyedconcepts.com>
2016-10-03 14:14:03 -0700

    remove gtm add analytics

--- inc/header-home.php
+++ inc/header-home.php
@@ -51,14 +51,6 @@
 
     <script type='text/javascript' data-cfasync='false' src='//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js' data-shr-siteid='e74b15b07b220b4f2dddb19ccaba9dae' async='async'></script>
 
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TNQSCZ');</script>
-    <!-- End Google Tag Manager -->
-
 </head>
 
 <body class="home">
